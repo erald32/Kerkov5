ce12037f5c94acc48a3a2663d2047461a2e6c2d5
erald32 <erald.kerlukU@gmail.com>
2016-07-20 12:35:49 +0200

    config file

---
+++ app/src/main/java/com/jeddigital/kerkotaxi/Configurations.java
@@ -0,0 +1,22 @@
+package com.jeddigital.kerkotaxi;
+
+/**
+ * Created by Erald.Kerluku on 7/20/2016.
+ */
+public class Configurations {
+
+    public static final String WEB_SERVICE_URL = "http://www.jeddigital.com/kerko_taxi/service/clients/clients.php";
+
+    public static final String SHARED_PREF_CLIENT = "client_prefs";
+
+    public static final String RESPONSE_ERROR = "error";
+    public static final String RESPONSE_SUCCESS = "Success";
+    public static final String BOOKING_NOT_VALID = "booking_not_valid";
+
+    public static final String BOOKING_ID_PREF = "BookingID";
+
+    public static final String ClIENT_ID_PREF = "id_e_klientit";
+    public static final String CLIENT_STATUS_ID_PREF = "1";
+    public static final String CLIENT_LATITUDE_PREF = "latitude klientit qe vjen nga serveri";
+    public static final String CLIENT_LONGITUDE_PREF = "longitude klientit qe vjen nga serveri";
+}
